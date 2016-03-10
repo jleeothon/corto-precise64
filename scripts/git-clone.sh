e24461777ab02041e0236f99f591e41bf8a401f2
Johnny Lee Othon <jleeothon@gmail.com>
2016-03-10 12:36:34 +0100

    Added git-clone.sh

---
+++ scripts/git-clone.sh
@@ -0,0 +1,10 @@
+cd cortoproject
+git clone https://github.com/cortoproject/corto
+git clone https://github.com/cortoproject/c-binding
+git clone https://github.com/cortoproject/xml
+git clone https://github.com/cortoproject/corto-language
+git clone https://github.com/cortoproject/io
+git clone https://github.com/cortoproject/cortodoc
+git clone https://github.com/cortoproject/test
+git clone https://github.com/cortoproject/json
+git clone https://github.com/cortoproject/python-binding
