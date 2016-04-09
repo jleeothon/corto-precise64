0cf63a48f0927a32d4f88ac6d4e3b3639b09101b
jleeothon <jleeothon@gmail.com>
2016-04-09 15:27:28 +0200

    Changed precise64 for trusty64 and git-update

--- scripts/git-update.sh
+++ scripts/git-update.sh
@@ -1,10 +1,10 @@
 cd cortoproject
-cd corto           && git checkout master && git pull && cd ..
-cd c-binding       && git checkout master && git pull && cd ..
-cd xml             && git checkout master && git pull && cd ..
-cd corto-language  && git checkout master && git pull && cd ..
-cd io              && git checkout master && git pull && cd ..
-cd cortodoc        && git checkout master && git pull && cd ..
-cd test            && git checkout master && git pull && cd ..
-cd json            && git checkout master && git pull && cd ..
-cd python-binding  && git checkout master && git pull && cd ..
+cd corto           && git checkout . && git checkout master && git pull && cd ..
+cd c-binding       && git checkout . && git checkout master && git pull && cd ..
+cd xml             && git checkout . && git checkout master && git pull && cd ..
+cd corto-language  && git checkout . && git checkout master && git pull && cd ..
+cd io              && git checkout . && git checkout master && git pull && cd ..
+cd cortodoc        && git checkout . && git checkout master && git pull && cd ..
+cd test            && git checkout . && git checkout master && git pull && cd ..
+cd json            && git checkout . && git checkout master && git pull && cd ..
+cd python-binding  && git checkout . && git checkout master && git pull && cd ..
